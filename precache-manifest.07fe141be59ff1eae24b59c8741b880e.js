b8b0941e553e6d57959979036828563081bf385d
premakanda <199301py@gmail.com>
2020-02-22 20:05:52 +0300

    Updates

---
+++ precache-manifest.07fe141be59ff1eae24b59c8741b880e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b157e87fcea1433d02ecf74e81c7acfb",
+    "url": "/counter/index.html"
+  },
+  {
+    "revision": "b69b754376c6b95b7b82",
+    "url": "/counter/static/css/main.5f48d026.chunk.css"
+  },
+  {
+    "revision": "ab99de08b6b00ed59581",
+    "url": "/counter/static/js/2.f5e0ee97.chunk.js"
+  },
+  {
+    "revision": "b69b754376c6b95b7b82",
+    "url": "/counter/static/js/main.e7d4e84a.chunk.js"
+  },
+  {
+    "revision": "584bf61ae5f144bfd524",
+    "url": "/counter/static/js/runtime-main.c3ed45ce.js"
+  }
+]);
